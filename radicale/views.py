fae18d80910039819d9995c8ee1cca300145d8c6
fanjunwei <fanjunwei9140@gmail.com>
2014-12-08 18:00:52 +0800

    删除无用代码

--- radicale/views.py
+++ radicale/views.py
@@ -37,29 +37,5 @@ def radicaleViewHandle(request, *args, **kwargs):
     return response
 
 
-class RadicaleView(View):
-    http_method_names = ['options', 'put', 'mkcol', 'head', 'get', 'delete', 'propfind', 'proppatch', 'copy', 'move',
-                         'lock', 'unlock']
-
-    @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
-        response = None
-
-        def start_response(status_code, headers):
-            response = HttpResponse()
-            response.status_code = status_code
-
-            for i in headers:
-                response[i[0]] = i[1]
-
-        content = app(request.META, start_response)
-        if response:
-            response.content = content
-        return response
-
-    def _allowed_methods(self):
-        return [m.upper() for m in self.http_method_names]
-
-
 
 
