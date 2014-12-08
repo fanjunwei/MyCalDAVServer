64fea69888b1a01e53f4459716167fdcc7be7399
fanjunwei <fanjunwei9140@gmail.com>
2014-12-08 18:04:37 +0800

    删除无用代码

--- MyCalDAVServer/urls.py
+++ MyCalDAVServer/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from radicale.views import RadicaleView, radicaleViewHandle
+from radicale.views import radicaleViewHandle
 
 admin.autodiscover()
 
